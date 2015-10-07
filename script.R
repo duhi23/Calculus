c11a66228229f041149a2c1feeee75c050086510
duhi23 <diego.21.phs@hotmail.com>
2015-10-06 23:56:08 -0500

    Script Pruebas

--- script.R
+++ script.R
@@ -1,16 +1,30 @@
 #### Script - Examenes aleatorios ####
 
+# Primera prueba
 setwd("./first_test")
 list.files()
 
-#pdf(filename="prueba01.pdf")
 for (i in seq(1:55)) {
       knitr::knit('first_test.Rnw', output = paste('prueba_', i, '.tex', sep = ''))
       system(paste('pdflatex ', 'prueba_', i, ".tex", sep=''))
 }
-#dev.off()
+
+# Primer deber
 
 for (i in seq(1:55)) {
       knitr::knit('first_task.Rnw', output = paste('deber_', i, '.tex', sep = ''))
       system(paste('pdflatex ', 'deber_', i, ".tex", sep=''))
 }
+
+# Segunda prueba
+
+dir.create("./second_test")
+setwd("./second_test")
+list.files()
+
+for (i in seq(1:55)) {
+      knitr::knit('second_test.Rnw', output = paste('prueba_', i, '.tex', sep = ''))
+      system(paste('pdflatex ', 'prueba_', i, ".tex", sep=''))
+}
+
+
